0a276ce3faabc59fbbf12249c551857fa3a78459
zurson <matu.jurek@gmail.com>
2024-06-07 16:14:54 +0200

    Now TopicData contains also producer id

--- src/main/java/org/example/server/TopicData.java
+++ src/main/java/org/example/server/TopicData.java
@@ -3,6 +3,6 @@ package org.example.server;
 import java.net.Socket;
 import java.util.Set;
 
-public record TopicData(Socket producer, Set<Socket> subscribers) {
+public record TopicData(Socket producer, String producerId, Set<Socket> subscribers) {
 
 }
